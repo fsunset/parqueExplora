24e53c8c98178c4bb714531e3d321382e209179d
Francisco Barahona <francisco.barahona.sarmiento@gmail.com>
2020-11-06 13:39:50 -0500

    WhatsApp btn.

--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -1,5 +1,8 @@
 import { useHistory } from "react-router-dom";
-
+import {
+  WhatsappShareButton,
+} from "react-share";
+import whatsAppImg from './img/whatsApp.png';
 import './sass/map.scss';
 
 const MapComponent = () => {
@@ -7,6 +10,17 @@ const MapComponent = () => {
 
   return (
     <div className="main__map">
+      {/* WhatsApp Contact Button */}
+      <a
+        href={`https://api.whatsapp.com/send/?phone=573152521451&text=Hola%2C+estoy+navegando+el+universo+explora&app_absent=0`}
+        className="whatsapp__btn"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={whatsAppImg} alt="WhatsApp Img" className="share-icon" />
+      </a>
+
+      {/* Sections */}
       <div
         className="main__map-universe"
         onClick={() => history.push("/explora-universo")}
